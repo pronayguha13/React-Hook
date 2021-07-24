522d715943d80a059f48c3eb641b44d4f5db71f0
pronayguha13 <pronayguha13@gmail.com>
2021-07-24 20:57:06 +0530

    Lesson Two

--- src/Components/HookCounter.js
+++ src/Components/HookCounter.js
@@ -1,11 +1,32 @@
 import React, { useState } from "react";
 
 const HookCounter = () => {
-  const [count, setCount] = useState(0);
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+
+  //set value based on previous state
+  const incrementFive = () => {
+    // for (let index = 0; index < 5; index++) {
+    //   setCount(count + 1);
+    // }
+
+    /*
+    setCount(count + 1) => is unsafe because it can happen that it is stale
+  */
+    setCount((prevCount) => prevCount + 5);
+  };
 
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Count:{count}</button>
+      count:{count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrement
+      </button>
+      <button onClick={incrementFive}>increment five</button>
     </div>
   );
 };
